cdeb02f542a6c4e5cae3a46f0e7b682afa793298
shagi <shagi@osoigo.com>
2025-06-25 16:12:44 +0200

    feat: máximo de apoyos para partidas presupuestarias

---
+++ app/models/custom/budget/investment.rb
@@ -0,0 +1,17 @@
+require_dependency Rails.root.join('app', 'models', 'budget', 'investment').to_s
+
+class Budget
+  class Investment
+    scope :enough_support, -> {
+      joins(:heading).where("budget_investments.cached_votes_up + budget_investments.physical_votes >= budget_headings.required_support")
+    }
+
+    def has_required_support?
+      if heading.required_support.present?
+        return heading.required_support <= total_votes
+      end
+      return false
+    end
+  end
+end
+
