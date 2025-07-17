966f0966b88c1546af54b134f31a3ab08d8c0fe3
Senén <senenrodero@gmail.com>
2025-07-17 18:55:36 +0200

    Add custom validations
    We found this validation in the old version code.

--- app/models/custom/verification/residence.rb
+++ app/models/custom/verification/residence.rb
@@ -1,4 +1,23 @@
 load Rails.root.join("app", "models", "verification", "residence.rb")
 
 class Verification::Residence
+  validate :document_number_length
+
+  private
+
+    def document_number_length
+      return if errors.any?
+
+      unless document_number_length_valid?
+        errors.add(:document_number, :wrong_length, count: 9)
+        store_failed_attempt
+        Lock.increase_tries(user)
+      end
+    end
+
+    def document_number_length_valid?
+      return false if (document_type != "2") && (!document_number.present? || (document_number.length != 9))
+
+      true
+    end
 end
