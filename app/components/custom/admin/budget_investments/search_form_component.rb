fc5ed2e17d3521d74905bc8c903eb20a96159684
shagi <shagi@osoigo.com>
2026-03-09 17:06:13 +0100

    feat: poder filtrar propuestas de inversión con comentarios o sin comentarios en admin

---
+++ app/components/custom/admin/budget_investments/search_form_component.rb
@@ -0,0 +1,7 @@
+class Admin::BudgetInvestments::SearchFormComponent < ApplicationComponent; end
+
+load Rails.root.join("app", "components", "admin", "budget_investments", "search_form_component.rb")
+
+class Admin::BudgetInvestments::SearchFormComponent
+  # Only template customizations
+end
--- app/models/custom/budget/investment.rb
+++ app/models/custom/budget/investment.rb
@@ -2,6 +2,27 @@ load Rails.root.join("app", "models", "budget", "investment.rb")
 
 class Budget
   class Investment
+    # Add sort by ballots
     SORTING_OPTIONS = { id: "id", supports: "cached_votes_up", ballots: "ballot_lines_count" }.freeze
+
+
+    def self.advanced_filters(params, results)
+      # Add filter by comments
+      results = results.without_admin      if params[:advanced_filters].include?("without_admin")
+      results = results.without_valuator   if params[:advanced_filters].include?("without_valuator")
+      results = results.under_valuation    if params[:advanced_filters].include?("under_valuation")
+      results = results.valuation_finished if params[:advanced_filters].include?("valuation_finished")
+      results = results.winners            if params[:advanced_filters].include?("winners")
+
+      ids = []
+      ids += results.valuation_finished_feasible.ids if params[:advanced_filters].include?("feasible")
+      ids += results.where(selected: true).ids       if params[:advanced_filters].include?("selected")
+      ids += results.undecided.ids                   if params[:advanced_filters].include?("undecided")
+      ids += results.unfeasible.ids                  if params[:advanced_filters].include?("unfeasible")
+      ids += results.where("comments_count > 0").ids if params[:advanced_filters].include?('with_comments')
+      ids += results.where("comments_count = 0").ids if params[:advanced_filters].include?('without_comments')
+      results = results.where(id: ids) if ids.any?
+      results
+    end
   end
 end
