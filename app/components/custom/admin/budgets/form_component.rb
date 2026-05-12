96de4fc86459e4b542a7b1f60b43e11a463e369d
Ales Zabala <shagi@osoigo.com>
2026-05-12 12:00:44 +0200

    Negative ballot lines (#18)
    ---------
    
    Co-authored-by: Ainhoa Gomez <ainhoa.gomez@osoigo.com>

---
+++ app/components/custom/admin/budgets/form_component.rb
@@ -0,0 +1,6 @@
+class Admin::Budgets::FormComponent < ApplicationComponent; end
+
+load Rails.root.join("app", "components", "admin", "budgets", "form_component.rb")
+
+class Admin::Budgets::FormComponent
+end
