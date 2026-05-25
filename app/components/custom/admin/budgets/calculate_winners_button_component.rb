dfb8293afa01674e4cf40e889a476fea7c34e1d4
shagi <shagi@osoigo.com>
2026-05-25 12:10:17 +0200

    feat: permitir asignar propuestas ganadoras manualmente

---
+++ app/components/custom/admin/budgets/calculate_winners_button_component.rb
@@ -0,0 +1,6 @@
+class Admin::Budgets::CalculateWinnersButtonComponent < ApplicationComponent ; end
+
+load Rails.root.join("app", "components", "admin", "budgets", "calculate_winners_button_component.rb")
+
+class Admin::Budgets::CalculateWinnersButtonComponent
+end
--- app/controllers/custom/admin/budgets_controller.rb
+++ app/controllers/custom/admin/budgets_controller.rb
@@ -8,4 +8,45 @@ class Admin::BudgetsController
       :negative_votes, :negative_vote_value
     ]
   end
+
+  def set_winners_form
+  end
+
+  def set_winners
+    return unless @budget.balloting_finished?
+
+    file_data = params[:csvfile]
+    if file_data.respond_to?(:read)
+      contents = file_data.read
+    elsif file_data.respond_to?(:path)
+      contents = File.read(file_data.path)
+    else
+      return
+    end
+
+    @budget.investments.update_all(winner: false)
+    winner_investments = []
+    missing_investment_ids = []
+    contents.split("\n").each do |line|
+      investment_id = line.strip
+      begin
+        winner_investments << Budget::Investment.find(investment_id)
+      rescue ActiveRecord::RecordNotFound
+        missing_investment_ids << investment_id
+      end
+    end
+
+    winner_investments.each do |investment|
+      investment.update(winner: true)
+    end
+    if missing_investment_ids.size > 0
+      alert_msg = I18n.t("admin.budgets.winners.missing_winner_ids", count: missing_investment_ids.size)
+      alert_msg = "%s: %s " % [alert_msg, missing_investment_ids.join(" , ")]
+      redirect_to admin_budget_budget_investments_path(budget_id: @budget.id, filter: "winners"),
+                  alert:  alert_msg
+    else
+      redirect_to admin_budget_budget_investments_path(budget_id: @budget.id, filter: "winners"),
+                  notice: I18n.t("admin.budgets.winners.done")
+    end
+  end
 end
