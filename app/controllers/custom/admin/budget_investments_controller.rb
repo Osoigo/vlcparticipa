f9170caf36b8f1e004df45e0a6a001acaf27ece8
shagi <shagi@osoigo.com>
2026-05-25 16:44:15 +0200

    feat: pestañas con contadores para el listado de propuestas de gasto en admin

--- app/controllers/custom/admin/budget_investments_controller.rb
+++ app/controllers/custom/admin/budget_investments_controller.rb
@@ -1,8 +1,12 @@
 load Rails.root.join("app", "controllers", "admin", "budget_investments_controller.rb")
 
 class Admin::BudgetInvestmentsController
+  has_filters %w[all without_admin without_valuator under_valuation valuation_finished enough_support not_enough_support winners],
+              only: :index
+
+  before_action :load_counters, only: [:index]
+
   def index
-    # TODO: intentar no sobreescribir por completo el método
     load_tags
     respond_to do |format|
       format.html
@@ -23,5 +27,17 @@ class Admin::BudgetInvestmentsController
       @investments = Budget::Investment.scoped_filter(params, @current_filter).order_filter(params)
       @investments = Kaminari.paginate_array(@investments) if @investments.is_a?(Array)
       @investments = @investments.page(params[:page]) unless request.format.csv? || request.format.xlsx?
+    end
+
+    def load_counters
+      @counts = Budget::Investment.scoped_filter(params, @current_filter)
+      @counts_total = @counts.count
+      @counts_without_admin = @counts.where(administrator_id: nil).count
+      @counts_without_val = @counts.where(valuator_assignments_count: 0).count
+      @counts_in_val = @counts.where(valuation_finished: false).count
+      @counts_aval_finished = @counts.where(valuation_finished: true).count
+      @counts_enough_support = @counts.enough_support.count
+      @counts_not_enough_support = @counts.not_enough_support.count
+      @counts_winners = @counts.where(winner: true).count
     end
 end
