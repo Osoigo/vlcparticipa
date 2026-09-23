29c857925207acca0c6a11b14e6b5f569ff80c22
Ainhoa Gomez <ainhoa.gomez@osoigo.com>
2026-09-23 13:20:27 +0200

    chore: Eliminar autor en el detalle del proyecto de gasto.

---
+++ app/components/custom/shared/detailed_info_component.rb
@@ -0,0 +1,6 @@
+class Shared::DetailedInfoComponent < ApplicationComponent; end
+
+load Rails.root.join("app", "components", "shared", "detailed_info_component.rb")
+
+class Shared::DetailedInfoComponent < ApplicationComponent
+end
