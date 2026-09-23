f22577d9e45ac6e68a413392cc3e67bac63ce6fc
Ainhoa Gomez <ainhoa.gomez@osoigo.com>
2026-09-23 13:23:00 +0200

    chore: Cambiar orden en detalle de proyecto de gasto para incluir investment_code y price_explanation al comienzo.

---
+++ app/components/custom/budgets/investments/investment_detail_component.rb
@@ -0,0 +1,6 @@
+class Budgets::Investments::InvestmentDetailComponent < ApplicationComponent; end
+
+load Rails.root.join("app", "components", "budgets", "investments", "investment_detail_component.rb")
+
+class Budgets::Investments::InvestmentDetailComponent < ApplicationComponent
+end
